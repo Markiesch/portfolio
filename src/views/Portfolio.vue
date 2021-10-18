6f0ccb621bf5ca06f74d010b6dfc759a858f0040
Markiesch <marksmeel@gmail.com>
2021-10-18 07:56:16 +0200

    Fixed router link being incorrect

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,20 +1,29 @@
 <template>
   <h1>Portfolio</h1>
+
+  <Breadcrumbs>Portfolio</Breadcrumbs>
+
   <section class="project--section">
     <article v-for="(project, index) of projects" :key="index">
-      <router-link to="/">
-        <img :src="require(`@/assets/mockups/${project.image}.png`)" :alt="project.title" />
+      <router-link :to="`/${project.name}`">
+        <img :src="require(`@/assets/${project.mockup ? 'mockups/' + project.mockup : 'projects/' + project.name + '/' + project.gallery[0]}.png`)" :alt="project.title" />
       </router-link>
     </article>
   </section>
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
-import Projects from "@/utils/projects";
+import { Vue, Options } from "vue-class-component";
+import Breadcrumbs from "@/components/Breadcrumbs.vue";
+import Portfolio from "@/utils/projects";
 
+@Options({
+  components: {
+    Breadcrumbs,
+  },
+})
 export default class Home extends Vue {
-  projects = Projects;
+  projects = Portfolio;
 }
 </script>
 
@@ -27,12 +36,17 @@ export default class Home extends Vue {
   article {
     border-radius: 10px;
     overflow: hidden;
-  }
 
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      transition: transform 300ms ease;
+    }
+
+    &:hover img {
+      transform: scale(1.1);
+    }
   }
 }
 </style>
