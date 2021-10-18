504f3492ebdf88be798bf51e0373695934675531
Markiesch <marksmeel@gmail.com>
2021-10-18 07:54:50 +0200

    Added gallery and name to project json

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,25 +1,40 @@
 export interface IProjects {
+  name: string;
   title: string;
-  image: string;
+  mockup?: string;
   description?: string;
+  gallery?: string[];
 }
 
 const projects: IProjects[] = [
   {
+    name: "tennis",
     title: "Tennisclub de Pettelear",
-    image: "pettelaer",
+    mockup: "pettelaer",
+    gallery: [""],
   },
   {
+    name: "dentist",
     title: "Dentist practice 's-Hertogenbosch",
-    image: "dentist",
+    mockup: "dentist",
+    gallery: [""],
   },
   {
+    name: "psv",
     title: "football club PSV",
-    image: "psv",
+    mockup: "psv",
+    gallery: [""],
   },
   {
+    name: "test",
     title: "KW1C study choice test",
-    image: "kw1c",
+    mockup: "kw1c",
+    gallery: [""],
+  },
+  {
+    name: "foodz",
+    title: "KW1C study choice test",
+    gallery: ["homepage", "login", "overzicht", "editor", "edit", "appearance"],
   },
 ];
 
