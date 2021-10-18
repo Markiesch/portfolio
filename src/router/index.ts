ae86c3b3853515dbe7bbe68570668708f8d41402
Markiesch <marksmeel@gmail.com>
2021-10-18 09:16:14 +0200

    Fixed issue where scroll position was saved during route switch

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,6 +26,9 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 
 export default router;
