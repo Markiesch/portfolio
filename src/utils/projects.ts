673e26400d72961e02c3b81c81f86536b4b138ed
Markiesch <marksmeel@gmail.com>
2021-10-28 13:48:55 +0200

    Added description to projects

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -2,6 +2,8 @@ export interface IProjects {
   name: string;
   title: string;
   date: string;
+  roles: string[];
+  client: string;
   mockup?: string;
   description?: string;
   gallery?: string[];
@@ -10,44 +12,62 @@ export interface IProjects {
 const projects: IProjects[] = [
   {
     name: "foodz",
-    title: "KW1C study choice test",
+    title: "Foodz - schoolproject",
     date: "March 2021",
+    roles: ["UI/UX", "Frontend", "Backend"],
+    client: "Koning Willem I College",
     mockup: "foodz",
+    description: "Foodz is a webapp that is builded for horeca students at Koning Willem I College (not for use purposes) where these students can view, add, edit and delete catering events.",
     gallery: ["homepage", "login", "overzicht", "editor", "edit", "appearance"],
   },
   {
     name: "notes",
     title: "Notes webapp",
     date: "March 2021",
+    roles: ["UI/UX", "Frontend"],
+    client: "Personal project",
     mockup: "notes",
+    description: "Notes is a web based personal project, started to dive a bit deeper in the world of VueJs with vuetify.",
     gallery: ["homepage", "editor", "favorite"],
   },
   {
     name: "memory",
-    title: "Notes webapp",
+    title: "Memory Game",
     date: "March 2021",
+    roles: ["UI/UX", "Frontend"],
+    client: "Personal project",
     mockup: "memory",
+    description: "The memory game is a pair matching game where the player has to click on 2 cards, if they are the same then they remain. The game ends whenever all cards are flipped successfully.",
     gallery: ["homepage", "game", "victory"],
   },
   {
     name: "pettelaer",
     title: "Tennisclub de Pettelear",
     date: "March 2021",
+    roles: ["UI/UX", "Frontend", "Backend"],
+    client: "Koning Willem I College",
     mockup: "pettelaer",
+    description: "A small website builded for members of the tennis club BTC De Pettelaer, where members cna view various information about other members.",
     gallery: ["homepage", "login", "overzicht"],
   },
   {
     name: "dentist",
     title: "Dentist practice 's-Hertogenbosch",
     date: "March 2021",
+    roles: ["UI/UX", "Frontend"],
+    client: "Koning Willem I College",
     mockup: "dentist",
+    description: "A small website made for patients at Tandarts Den Bosch where patients can do a test to rechieve a brushscore.",
     gallery: ["homepage", "question", "print"],
   },
   {
     name: "studytest",
     title: "KW1C study choice test",
     date: "March 2021",
+    roles: ["UI/UX", "Frontend", "Backend"],
+    client: "Koning Willem I College",
     mockup: "kw1c",
+    description: "A web based studyselftest made for students at Koning Willem I College.",
     gallery: ["homepage", "form-name", "form-question", "result"],
   },
 ];
