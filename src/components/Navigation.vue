200280384a73570604ee931a8ebc4eb5a8f65112
Markiesch <marksmeel@gmail.com>
2021-11-10 22:42:29 +0100

    Added hamburger menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,21 +1,41 @@
 <template>
-  <header>
+  <header class="navigation">
     <nav>
       <router-link to="/" exact class="logo--container">
         <img src="../assets/logo.png" alt="logo" />
       </router-link>
-      <div>
+      <div class="link--container">
         <router-link to="/" exact>Home</router-link>
         <router-link to="/portfolio">Portfolio</router-link>
         <!-- <router-link to="/Resume">Resume</router-link> -->
         <router-link to="/contact">Contact</router-link>
       </div>
+      <div @click="menuOpen = !menuOpen" class="burger--menu__container">
+        <div class="burger--menu"></div>
+      </div>
     </nav>
   </header>
+
+  <div class="menu" :class="{ open: menuOpen }">
+    <nav>
+      <router-link to="/">home</router-link>
+      <router-link to="/portfolio">portfolio</router-link>
+      <router-link to="/resume">resume</router-link>
+      <router-link to="/contact">contact</router-link>
+    </nav>
+  </div>
 </template>
 
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Navigation extends Vue {
+  menuOpen = false;
+}
+</script>
+
 <style lang="scss">
-header {
+.navigation {
   position: fixed;
   z-index: 2;
   width: 100%;
@@ -36,13 +56,95 @@ header {
     align-items: center;
   }
 
-  div {
+  .link--container {
     display: flex;
     gap: 2rem;
   }
 
+  .burger--menu__container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 5rem;
+    height: 100%;
+    cursor: pointer;
+
+    .burger--menu::before,
+    .burger--menu::after,
+    .burger--menu {
+      width: 2.5rem;
+      height: 0.25rem;
+      border-radius: 0.25rem;
+      transition: transform 600ms ease, background-color 600ms ease;
+      background-color: var(--primary-color);
+    }
+
+    .burger--menu::before,
+    .burger--menu::after {
+      content: "";
+      position: absolute;
+    }
+
+    .burger--menu::before {
+      transform: translateY(-0.75rem);
+    }
+
+    .burger--menu::after {
+      transform: translateY(0.75rem);
+    }
+
+    &:hover .burger--menu::before {
+      transform: translateY(-0.9rem);
+    }
+
+    &:hover .burger--menu::after {
+      transform: translateY(0.9rem);
+    }
+  }
+
   .router-link-active {
     font-weight: bold;
+  }
+}
+
+.menu {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 100vh;
+  background-color: var(--primary-color);
+  color: white;
+  transition: transform 300ms ease;
+  transform: translateY(-100%);
+
+  &.open {
+    transform: translateY(0);
+  }
+
+  a {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+    display: block;
+    text-align: center;
+    opacity: 0.4;
+  }
+
+  .router-link-active,
+  a:hover {
+    opacity: 1;
+  }
+}
+
+@media screen and (max-width: 50rem) {
+  .navigation {
+    .link--container {
+      display: none;
+    }
   }
 }
 </style>
