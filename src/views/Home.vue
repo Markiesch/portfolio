2fe7d8f833a5715b5dd3e097bccda759ee1ef3a9
Markiesch <marksmeel@gmail.com>
2021-11-13 20:53:31 +0100

    redesigned hero and about me section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,90 +2,151 @@
   <h1>developer</h1>
   <section class="hero">
     <article>
-      <h3>JUNIOR SOFTWARE DEVELOPER</h3>
-      <h2>I Build Awesome Experience</h2>
-      <router-link to="/portfolio">View Projects</router-link> -
-      <router-link to="/">Contact me</router-link>
-    </article>
-    <article>
-      <img src="../assets/character.png" alt="character" />
+      <div class="image--container">
+        <img src="../assets/character.png" alt="character" />
+      </div>
+      <p class="fade-in">Hi! I'm Mark👋,</p>
+      <h2 class="fade-in">I build awesome experience</h2>
+      <div class="fade-in cta">
+        <router-link class="portfolio--link" to="/portfolio">
+          <svg viewBox="0 0 576 512" class="svg-inline--fa fa-eye fa-w-18 fa-9x">
+            <path
+              d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"
+            ></path>
+          </svg>
+          <span>View Projects</span>
+        </router-link>
+        <router-link to="/">
+          <svg viewBox="0 0 24 24">
+            <path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z" />
+          </svg>
+          <span>Contact me</span>
+        </router-link>
+      </div>
     </article>
   </section>
+
   <section class="project--section">
     <article>
-      <h3>FEATURED PROJECT</h3>
       <h2>{{ projects[0].title }}</h2>
       <p>{{ projects[0].description }}</p>
     </article>
     <article>
-      <img :src="require(`@/assets/mockups/${projects[0].mockup}.png`)" alt="Project" />
+      <router-link :to="`/project/${projects[0].name}`">
+        <img :src="require(`@/assets/mockups/${projects[0].mockup}.png`)" alt="Project" />
+      </router-link>
     </article>
   </section>
+
   <section class="about--section">
-    <article>
-      <p class="heading"><span>Hey!</span> I am Mark Schuurmans,</p>
-      <img src="../assets/character.png" alt="character" />
+    <article class="image--container">
+      <img src="../assets/character2.png" alt="character" />
     </article>
     <article>
+      <p class="heading">Hey! I'm Mark👋,</p>
       <p>
-        a 17 years old student at the Koning Willen I College, where I study to become a Software Developer. I am currently in my first year of this 4-year-old BOL education. I am currently working
-        with HTML, CSS, Javascript, PHP, SQL, and VueJS. But when I am not coding or shifting pixels around, I fill my free time by playing video games and basketball.
+        a 17 years old front-end development enthusiast with 2+ years of experience. I am currently living and studying in 's-Hertogenbosch, the Netherlands. I am currently work with HTML, CSS,
+        Javascript, PHP, SQL, and VueJS. But when I am not coding or shifting pixels around, I fill my free time by playing video games and basketball.
       </p>
+      <router-link to="/portfolio"></router-link>
     </article>
   </section>
+
   <section class="contact--section">
     <h2>Interested to work with me?</h2>
-    <button>let's talk</button>
+    <router-link to="/contact">Let's work together</router-link>
   </section>
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-class-component";
-import Portfolio from "@/utils/projects";
+import portfolio from "@/utils/projects";
 
 export default class Home extends Vue {
-  projects = Portfolio;
+  projects = portfolio;
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .hero {
   display: flex;
+  justify-content: center;
   align-items: center;
   flex-wrap: wrap;
-  gap: 1rem;
+  text-align: center;
   min-height: calc(100vh - var(--header-height));
   padding-bottom: var(--header-height);
 
-  article {
-    flex: 1 1 25rem;
-  }
-
-  article + article {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  .image--container {
+    height: 8rem;
+    margin-bottom: 1rem;
+    display: block;
   }
 
   img {
-    max-width: 25rem;
-    width: 90%;
+    height: 100%;
   }
-}
 
-h2 {
-  margin-bottom: 0.5em;
+  p {
+    color: var(--primary-color);
+    background-color: hsla(220, 33%, 10%, 0.05);
+    padding: 0.5rem 0.75rem;
+    border-radius: 0.25rem;
+    display: inline-block;
+    margin: 0 auto;
+  }
+
+  h2 {
+    font-weight: 600;
+    animation-delay: 300ms;
+    max-width: 17ch;
+    margin: 1rem auto 3rem auto;
+  }
+
+  .cta {
+    animation-delay: 600ms;
+  }
+
+  a {
+    font-size: 1.25rem;
+    padding: 0.75rem;
+    border-radius: 0.25rem;
+    display: inline-flex;
+    font-weight: 300;
+    align-items: center;
+  }
+
+  svg {
+    height: 1.5rem;
+    margin-right: 0.5rem;
+    fill: currentColor;
+  }
+
+  .portfolio--link {
+    background-color: var(--primary-color);
+    margin-right: 1rem;
+    color: white;
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.2);
+  }
 }
 
 .project--section {
   display: flex;
-  flex-wrap: wrap;
   align-items: center;
-  gap: 2rem;
-  margin-bottom: 5rem;
+  flex-wrap: wrap;
+  padding: 0;
 
   article {
+    margin: 1rem;
     flex: 1 1 25rem;
+  }
+
+  h2 {
+    margin-bottom: 1rem;
+  }
+
+  p {
+    color: var(--grey-color);
   }
 
   img {
@@ -93,52 +154,95 @@ h2 {
     display: block;
     box-shadow: 0.5rem 0.5rem 2rem rgba(0, 0, 0, 0.2);
     border-radius: 0.75rem;
+    transition: transform 300ms ease;
+
+    &:hover {
+      transform: translateX(-2.5%) scale(1.025);
+    }
   }
 }
 
 .about--section {
   display: grid;
-  gap: 2rem;
-  grid-template-columns: auto 1fr;
+  align-items: center;
+  padding: 5rem 0;
+  min-height: 60vh;
+  grid-template-columns: 1fr auto;
+  color: var(--primary-color);
 
-  .heading {
-    color: #222;
+  article {
+    padding: 0 1rem;
   }
 
-  span {
-    position: relative;
-  }
-
-  span::after {
-    content: "";
-    position: absolute;
-    top: 100%;
-    height: 0.5rem;
-    background-color: var(--secondary-color);
-    border-radius: 1rem;
-    left: 0;
-    width: 100%;
+  .image--container {
+    width: 25rem;
+    max-width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   img {
-    max-width: 12.5rem;
-    margin: 1.5em auto 0 auto;
-    display: block;
+    max-width: 15rem;
+    width: 90%;
+  }
+
+  .heading {
+    background-color: var(--primary-color);
+    font-size: 1rem;
+    display: inline-block;
+    padding: 1em 1.25em;
+    line-height: 1;
+    margin-bottom: 1rem;
+    color: white;
+    font-weight: 300;
+    position: relative;
+    border-radius: 0.25rem;
+
+    &::after {
+      content: "";
+      position: absolute;
+      right: 100%;
+      top: 50%;
+      transform: translateY(-50%);
+      border: 0.5rem solid transparent;
+      border-right-color: var(--primary-color);
+    }
   }
 
   p {
-    color: var(--grey-color);
+    font-weight: 400;
+    line-height: 1.5;
   }
 }
 
 .contact--section {
-  margin: 10rem auto;
+  padding-bottom: 5rem;
   text-align: center;
+
+  a {
+    display: inline-block;
+    margin-top: 2rem;
+    font-size: 3rem;
+    padding: 0.5rem 0.75rem;
+    border-radius: 0.25rem;
+    font-weight: 500;
+    color: var(--primary-color);
+    text-decoration: underline;
+  }
 }
 
 @media screen and (max-width: 55rem) {
   .hero {
     text-align: center;
+  }
+
+  .about--section {
+    grid-template-columns: 1fr;
+
+    img {
+      display: none;
+    }
   }
 }
 </style>
