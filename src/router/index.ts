3e97e42a2968f03fab616e6855c1226eba777cf3
Markiesch <marksmeel@gmail.com>
2021-11-18 21:26:25 +0100

    Added webpackChunkName (Lazy loading)

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,27 +4,27 @@ const routes: RouteRecordRaw[] = [
   {
     path: "/",
     name: "Home",
-    component: () => import("@/views/Home.vue"),
+    component: () => import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
   },
   {
     path: "/portfolio",
     name: "portfolio",
-    component: () => import("@/views/Portfolio.vue"),
+    component: () => import(/* webpackChunkName: "portfolio" */ "@/views/Portfolio.vue"),
   },
   {
     path: "/project/:project",
     name: "project",
-    component: () => import("@/views/Project.vue"),
+    component: () => import(/* webpackChunkName: "project" */ "@/views/Project.vue"),
   },
   {
     path: "/contact",
     name: "contact",
-    component: () => import("@/views/Contact.vue"),
+    component: () => import(/* webpackChunkName: "contact" */ "@/views/Contact.vue"),
   },
   {
     path: "/:pathMatch(.*)*",
     name: "404",
-    component: () => import("@/views/404.vue"),
+    component: () => import(/* webpackChunkName: "404" */ "@/views/404.vue"),
   },
 ];
 
