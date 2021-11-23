f06c7c804a349bd948e59f886b0427d6517b8f9e
Markiesch <marksmeel@gmail.com>
2021-11-23 21:06:15 +0100

    Replaced root paths with absolute paths

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import Breadcrumbs from "@/components/Breadcrumbs.vue";
+import Breadcrumbs from "./components/Breadcrumbs.vue";
 
 createApp(App)
   .component("Breadcrumbs", Breadcrumbs)
