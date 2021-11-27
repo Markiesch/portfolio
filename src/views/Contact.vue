06741f41efc1e61c054a316b3748ed4df7b3ac79
Markiesch <marksmeel@gmail.com>
2021-11-27 22:27:31 +0100

    Improved contact section

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -9,51 +9,51 @@
   <section class="contact--section">
     <article class="information">
       <h2 class="section--title">Get in touch</h2>
-      <a href="https://www.google.com/maps/place/Den+Dungen" target="_blank">
-        <svg viewBox="0 0 384 512">
-          <path
-            d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"
-          ></path>
-        </svg>
-        <span>Den Dungen, The Netherlands</span>
-      </a>
-      <a href="https://www.discord.com/" target="_blank">
-        <svg viewBox="0 0 640 512">
-          <path
-            d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"
-          ></path>
-        </svg>
-        <span>Markiesch#4824</span>
-      </a>
-      <a href="mailto:marksmeel@gmail.com">
+      <p>Got a project? Drop me a line if you want to work together on something exciting. Big or small. Mobile or web.</p>
+      <div>
         <svg viewBox="0 0 512 512">
           <path
             d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"
           ></path>
         </svg>
-        <span>marksmeel@gmail.com</span>
-      </a>
+        <a href="mailto:marksmeel@gmail.com">marksmeel@gmail.com</a>
+      </div>
+      <div>
+        <svg viewBox="0 0 496 512">
+          <path
+            d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+          ></path>
+        </svg>
+        <a href="https://www.github.com/Markiesch" target="_blank">Github</a>
+      </div>
+      <div>
+        <svg viewBox="0 0 640 512">
+          <path
+            d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"
+          ></path>
+        </svg>
+        <a href="https://www.discord.com/" target="_blank">Markiesch#4824</a>
+      </div>
     </article>
     <article>
       <form @submit.prevent="submitForm" name="contact" method="POST" data-netlify-honeypot="bot-field" netlify>
         <div style="display: none;">
           <label>Do not fill this if you are a human: <input name="bot-field"/></label>
         </div>
-        <div class="form">
-          <div>
-            <label for="name">Name</label>
-            <input v-model="form.name" type="text" id="name" name="name" />
-          </div>
 
-          <div>
-            <label for="email">Email</label>
-            <input v-model="form.mail" type="text" id="email" name="mail" />
-          </div>
+        <label for="name">Name</label>
+        <div class="input--container">
+          <input v-model="form.name" type="text" id="name" name="name" />
+        </div>
 
-          <div>
-            <label for="name">Message</label>
-            <textarea v-model="form.message" id="name" name="message" />
-          </div>
+        <label for="email">E-mail</label>
+        <div class="input--container">
+          <input v-model="form.email" type="text" id="email" name="email" />
+        </div>
+
+        <label for="name">Message</label>
+        <div class="input--container">
+          <textarea v-model="form.message" id="name" name="message" />
         </div>
 
         <div v-show="error" class="alert">{{ error }}</div>
@@ -76,7 +76,7 @@ import { Map, Marker } from "mapbox-gl";
 export default class Home extends Vue {
   form = {
     name: "",
-    mail: "",
+    email: "",
     message: "",
   };
   error = "";
@@ -107,7 +107,7 @@ export default class Home extends Vue {
     const mailRegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (this.form.name.length < 3) return (this.error = "Name must be atleast 2 characters long");
-    if (!mailRegExp.test(this.form.mail.toLowerCase())) return (this.error = "Please enter a valid email");
+    if (!mailRegExp.test(this.form.email.toLowerCase())) return (this.error = "Please enter a valid email");
     if (this.form.message.length < 20) return (this.error = "Please enter a message that has atleast 20 characters");
 
     // Passed checks
@@ -119,7 +119,7 @@ export default class Home extends Vue {
       });
       this.form = {
         name: "",
-        mail: "",
+        email: "",
         message: "",
       };
     } catch (error) {
@@ -140,44 +140,49 @@ export default class Home extends Vue {
 .contact--section {
   display: flex;
   flex-wrap: wrap;
-  padding-top: 5rem;
+  padding: 5rem 0;
 
   article {
     flex: 1 1 20rem;
+    padding: 0 1rem;
     width: 100%;
   }
 
   .information {
     flex: 1 1 10rem;
+
+    p {
+      margin: 1rem 0 2rem 0;
+    }
+
+    div {
+      display: flex;
+      margin-top: 0.75rem;
+      align-items: center;
+      max-width: 20rem;
+      border-radius: 0.5rem;
+      color: var(--primary-color);
+      padding: 0.75rem 1rem;
+      background-color: hsla(220, 33%, 10%, 0.05);
+    }
+
+    svg {
+      flex: 0 0 1.5rem;
+    }
+
+    a {
+      padding-left: 0.75rem;
+    }
   }
 
   svg {
     fill: var(--primary-color);
     height: 1.4rem;
+  }
+
+  form {
+    max-width: 30rem;
     margin: 0 auto;
-  }
-
-  a {
-    display: grid;
-    grid-template-columns: 2rem 1fr;
-    margin-top: 0.75rem;
-    align-items: center;
-    color: var(--primary-color);
-
-    span {
-      padding-left: 0.75rem;
-    }
-  }
-
-  .form {
-    display: flex;
-    flex-wrap: wrap;
-
-    div {
-      flex: 1 1 20rem;
-      margin-bottom: 1em;
-      padding: 0.25rem;
-    }
   }
 
   input,
@@ -189,24 +194,48 @@ export default class Home extends Vue {
   }
 
   label {
+    margin: 1rem 0 0.25rem 0;
     font-weight: 500;
     cursor: pointer;
-    color: var(--primary-color);
   }
 
   input,
   textarea {
-    padding: 0.5em;
-    border: 1px solid rgba(120, 120, 120, 0.5);
+    position: relative;
+    padding: 0.75em;
     border-radius: 0.25rem;
     width: 100%;
+    background-color: hsla(220, 33%, 10%, 0.04);
+    overflow: hidden;
+  }
+
+  .input--container {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .input--container::after {
+    content: "";
+    position: absolute;
+    background-color: var(--primary-color);
+    height: 1px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    transform: translateX(-100%);
+    transition: 300ms transform ease;
+  }
+
+  .input--container:focus-within::after {
+    transform: translateX(0);
   }
 
   textarea {
     margin: 0 !important;
     resize: vertical;
-    height: 4rem;
-    max-height: 12.5;
+    min-height: 3rem;
+    height: 7rem;
+    max-height: 20rem;
   }
 
   .alert {
@@ -219,36 +248,23 @@ export default class Home extends Vue {
   }
 
   .send--button {
-    position: relative;
     display: inline-flex;
     align-items: center;
+    justify-content: center;
     font-size: 1rem;
-    font-weight: 400;
     border: none;
-    margin: 0.25rem;
+    margin-top: 1rem;
     cursor: pointer;
     border-radius: 0.25rem;
     background: none;
-    color: var(--primary-color);
-
-    &::after {
-      content: "";
-      position: absolute;
-      top: 105%;
-      left: 0;
-      width: 100%;
-      height: 0.2rem;
-      border-radius: 1rem;
-      background-color: transparent;
-      transition: background-color 300ms ease;
-    }
-
-    &:hover::after {
-      background-color: var(--primary-color);
-    }
+    color: white;
+    background-color: var(--primary-color);
+    width: 100%;
+    padding: 0.75rem;
 
     svg {
       margin-right: 0.5rem;
+      fill: white;
     }
   }
 }
