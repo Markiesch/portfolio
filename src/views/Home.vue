ebd2c4b0fcb875967ad2f5ff0a8ef41ad5cfb6b7
Markiesch <marksmeel@gmail.com>
2022-01-09 16:32:42 +0100

    FIxed alignment issues on mobile view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
       <h2 class="slide-in subheader">a <span>Software Developer</span> with an eye for <span>UI design</span>. I am specialized in UI/UX design and responsive web development.</h2>
       <div class="slide-in cta">
         <router-link class="portfolio--link" to="/portfolio">
-          <svg viewBox="0 0 576 512" class="svg-inline--fa fa-eye fa-w-18 fa-9x">
+          <svg viewBox="0 0 576 512">
             <path
               d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"
             ></path>
@@ -116,6 +116,7 @@ import projects from "../utils/projects";
     display: inline-flex;
     font-weight: 300;
     align-items: center;
+    margin: 0.5rem;
   }
 
   svg {
@@ -126,7 +127,6 @@ import projects from "../utils/projects";
 
   .portfolio--link {
     background-color: var(--primary-color);
-    margin-right: 1rem;
     color: white;
     box-shadow: 0 0 1rem rgba(0, 0, 0, 0.2);
   }
