a888a528be0d82ab91c780019c2e89b76f7fb267
Markiesch <marksmeel@gmail.com>
2022-06-04 13:35:57 +0200

    Updated project call to action

--- pages/index.vue
+++ pages/index.vue
@@ -30,25 +30,13 @@
       <div v-for="project of projects.slice(0, 3)">
         <NuxtLink class="image--container" :to="`/project/${project.name}`">
           <img :src="`/assets/mockups/${project.mockup}.png`" :alt="project.name" />
-          <div class="overlay">View Project</div>
         </NuxtLink>
         <div class="project--information primary">
           <h2>{{ project.title }}</h2>
           <p>{{ project.description }}</p>
-          <div v-if="project.links" class="cta--container">
-            <a :href="project.links[0].url" target="_blank" rel="noopener">
-              <svg viewBox="0 0 24 24">
-                <path
-                  d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"
-                ></path>
-              </svg>
-            </a>
-            <a :href="project.links[1].url" target="_blank" rel="noopener" class="demo--container">
-              <span>Visit website</span>
-              <svg viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-              </svg>
-            </a>
+          <div class="link--container">
+            <a :href="project.links[1].url" target="_blank" rel="noopener" class="learn__more--link">Learn more</a>
+            <a :href="project.links[1].url" target="_blank" rel="noopener" class="demo--link">Visit website</a>
           </div>
         </div>
       </div>
@@ -69,8 +57,9 @@
       <article>
         <p class="heading">Hey! I'm Mark👋,</p>
         <p>
-          a 17 year old front-end development enthusiast with 1+ years of experience. I currently live and study in 's-Hertogenbosch, the Netherlands. Learning and working with HTML, CSS, Javascript,
-          PHP, SQL, and VueJS is daily business for me, but when I am not coding or shifting pixels around, I like to fill my free time by playing video games and basketball.
+          a 18 year old front-end development enthusiast with 2+ years of experience. I currently live and study in 's-Hertogenbosch, the Netherlands. Learning and working with front-end tools such as
+          HTML, CSS, Javascript, and VueJS and backend technologies such as SQL and C# is daily business for me, but when I am not coding or shifting pixels around, I like to fill my free time by
+          playing video games and basketball.
         </p>
       </article>
     </section>
@@ -174,27 +163,11 @@ import projects from "../utils/projects";
 
   .image--container {
     position: relative;
-  }
+    transition: transform 300ms ease;
 
-  .overlay {
-    opacity: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.4);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition: 300ms ease;
-    border-radius: 1rem;
-    color: white;
-    font-size: 2rem;
-  }
-
-  .image--container:hover .overlay {
-    opacity: 1;
+    &:hover {
+      transform: rotateY(10deg) rotateX(10deg);
+    }
   }
 
   img {
@@ -219,20 +192,24 @@ import projects from "../utils/projects";
     line-height: 1.4;
   }
 
-  .cta--container {
-    display: flex;
-    align-items: center;
-    margin-top: 3rem;
-  }
+  .link--container {
+    margin-top: 2rem;
 
-  .demo--container {
-    margin-left: 1rem;
-    display: flex;
-    align-items: center;
+    a {
+      font-size: 0.9rem;
+      font-weight: 500;
+      padding: 0.75rem 1rem;
+      border-radius: 0.25rem;
+      display: inline-block;
 
-    span {
       margin-right: 1rem;
     }
+  }
+
+  .learn__more--link {
+    background-color: var(--primary-color);
+    color: white;
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.2);
   }
 
   svg {
