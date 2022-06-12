6b9365b0962d7a0b6d7c9baf6037cf291a8d2376
Markiesch <marksmeel@gmail.com>
2022-06-12 16:22:22 +0200

    Fixed font import render blocking

--- app.vue
+++ app.vue
@@ -13,7 +13,11 @@
     <!-- Google Font imports -->
     <Link rel="preconnect" href="https://fonts.googleapis.com" />
     <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-    <Link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+    <Link
+      rel="preload"
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+      onload="this.onload=null;this.rel='stylesheet'"
+    />
 
     <!-- Favicon configuration -->
     <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
